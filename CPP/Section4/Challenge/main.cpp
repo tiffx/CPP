1f080af926e235542541082609a46e7a02cc85ee
Tiffany Xiong <60724646+tiffx@users.noreply.github.com>
2021-02-11 12:23:00 -0700

    Delete CPP directory

--- CPP/Section4/Challenge/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int favorite_number;
-    std::cout << "Enter your favorite number between 1 and 100: ";
-    std::cin >> favorite_number;
-    std::cout << "Amazing that's my favorite number too! \nNo really!!, " << favorite_number << " is my favorite number!" << std::endl;
-    return 0;
-}
--- CPP/Section4/firstprogram.py
+++
@@ -1,2 +0,0 @@
-favorite_number = int(input("Enter your favorite number between 1 and 100: "))
-print(f"Amazing!! {favorite_number} is my favorite number too!")
